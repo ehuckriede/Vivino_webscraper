55f79bfd7406aa462bc3b22ae6f196825d67e81c
juliehabets <j.n.g.habets@tilburguniversity.edu>
2021-10-12 17:02:23 +0200

    changed average.rating variable from character to numeric

--- analysis.R
+++ analysis.R
@@ -4,8 +4,14 @@ library(dplyr)
 # Load the data
 all_data <- read.csv(file = 'All_regions_data_test.csv')
 
-# Check for outliers
+# Check for any anomalities 
 summary(all_data)
+
+# Change Average.Rating into a numerical variable 
+all_data$Average.Rating <- (gsub("\\,", ".", all_data$Average.Rating))
+all_data$Average.Rating <- as.numeric(all_data$Average.Rating)
+# Check whether it worked
+is.numeric(all_data$Average.Rating)
 
 # Get rid of columns that don't matter to us
 all_data <- all_data[, c(2, 4:7)]
@@ -18,3 +24,10 @@ all_data$piemonte <- as.numeric(all_data$Region == "Piemonte")
 all_data$rhone_valley <- as.numeric(all_data$Region == "Rhone Valley")
 all_data$toscana <- as.numeric(all_data$Region == "Toscana")
 
+# Check for NAs
+sum(is.na(all_data$Region))
+sum(is.na(all_data$Number.of.Ratings))
+sum(is.na(all_data$Average.Rating))
+
+# Mean of every region 
+all_data %>% group_by(Region) %>% summarize(average_rating = mean(Average.Rating))
