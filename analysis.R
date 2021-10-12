e68b10b83328d580d52d01ad81c28dc6681aef77
unknown <m.verbruggen_2@tilburguniversity.edu>
2021-10-12 22:27:56 +0200

    Delete filtered files from github and add final output file with lowercase letters

--- analysis.R
+++ analysis.R
@@ -2,32 +2,33 @@
 library(dplyr)
 
 # Load the data
-all_data <- read.csv(file = 'All_regions_data_test.csv')
+all_data <- read.csv(file = 'all_regions_data.csv')
 
 # Check for any anomalities 
+View(all_data)
 summary(all_data)
 
 # Change Average.Rating into a numerical variable 
-all_data$Average.Rating <- (gsub("\\,", ".", all_data$Average.Rating))
-all_data$Average.Rating <- as.numeric(all_data$Average.Rating)
+all_data$average_rating <- (gsub("\\,", ".", all_data$average_rating))
+all_data$average_rating <- as.numeric(all_data$average_rating)
 # Check whether it worked
-is.numeric(all_data$Average.Rating)
+is.numeric(all_data$average_rating)
 
 # Get rid of columns that don't matter to us
-all_data <- all_data[, c(2, 4:7)]
+all_data <- all_data[, c(2:6)]
 
 # Make dummy variables
-all_data$bordeaux <- as.numeric(all_data$Region == "Bordeaux")
-all_data$bourgogne <- as.numeric(all_data$Region == "Bourgogne")
-all_data$napa_valley <- as.numeric(all_data$Region == "Napa Valley")
-all_data$piemonte <- as.numeric(all_data$Region == "Piemonte")
-all_data$rhone_valley <- as.numeric(all_data$Region == "Rhone Valley")
-all_data$toscana <- as.numeric(all_data$Region == "Toscana")
+all_data$bordeaux <- as.numeric(all_data$region == "Bordeaux")
+all_data$bourgogne <- as.numeric(all_data$region == "Bourgogne")
+all_data$napa_valley <- as.numeric(all_data$region == "Napa Valley")
+all_data$piemonte <- as.numeric(all_data$region == "Piemonte")
+all_data$rhone_valley <- as.numeric(all_data$region == "Rhone Valley")
+all_data$toscana <- as.numeric(all_data$region == "Toscana")
 
 # Check for NAs
-sum(is.na(all_data$Region))
-sum(is.na(all_data$Number.of.Ratings))
-sum(is.na(all_data$Average.Rating))
+sum(is.na(all_data$region))
+sum(is.na(all_data$number_of_ratings))
+sum(is.na(all_data$average_rating))
 
 # Mean of every region 
-all_data %>% group_by(Region) %>% summarize(average_rating = mean(Average.Rating))
+all_data %>% group_by(region) %>% summarize(average_rating = mean(average_rating))
